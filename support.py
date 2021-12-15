6a0152af9b26a290cdc61f007e0848a43ffd6e4d
lakshmanp21 <lakshmanp@tradeix.com>
2021-12-15 13:34:45 +0000

    moved to correct folders

--- support.py
+++
@@ -1,8 +0,0 @@
-import math
-
-
-def factorialtest(test_string):
-    search_list = ['[', ']', '{', '}', '@', '_', '!', '"', '£', '$', '%', '^', '&', '*', '(', ')', '|', '~', '#', '<', '>', '?', '/', '\\', ':', ';', '.', '[', ']', '¬', '`']
-    if search_list not in test_string:
-        if test_string.isdecimal() is not None:
-            return math.factorial(test_string)
